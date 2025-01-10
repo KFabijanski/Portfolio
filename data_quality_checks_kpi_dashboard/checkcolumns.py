8ff5a56624270491b25d065e813d3d16128d8fac
KFabijanski <134225829+KFabijanski@users.noreply.github.com>
2025-01-10 22:07:52 +0100

    Update and rename sprawdzkolumny.py to checkcolumns.py

--- data_quality_checks_kpi_dashboard/checkcolumns.py
+++ data_quality_checks_kpi_dashboard/checkcolumns.py
@@ -12,23 +12,23 @@ from collections import Counter
 def count_columns_in_excel_files(folder_path):
     column_counter = Counter()
 
-    # Przechodzenie przez wszystkie pliki w folderze i podfolderach
+    # Traversing through all files in the folder and subfolders.
     for root, dirs, files in os.walk(folder_path):
         for file in files:
             if ('contacts' in file) and file.endswith('.xlsx') or file.endswith('.xls'):
                 file_path = os.path.join(root, file)
                 try:
-                    # Odczyt pliku Excel
+                    # Reading an Excel file.
                     df = pd.read_excel(file_path)
-                    # Zliczanie kolumn
+                    # Counting columns.
                     column_counter.update(df.columns)
                 except Exception as e:
                     print(f'Nie można odczytać pliku {file_path}: {e}')
 
-    # Wyświetlanie wyników
+    # Displaying results.
     for column, count in column_counter.items():
         print(f"{column} - ilość: {count}")
 
-# Ustaw ścieżkę do folderu z plikami Excel
-folder_path = r"C:\Users\fabijans-1\OneDrive - Mettler Toledo LLC\All units task list\Data Quality\Weekly checks\Archive"
+# Set the path to the folder with Excel files.
+folder_path = r"folderpathwithfiles"
 count_columns_in_excel_files(folder_path)
